48a338051a616f7cc635b36bbadabf2b1f31d20d
Arkiralor <prithoo11335@gmail.com>
2023-04-30 20:13:37 +0530

    reqorked test yaml

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
-python -m pip install --no-cache pip-tools
+python -m pip install pip-tools
 pip-compile
-python -m pip install --no-cache -r requirements.txt
+python -m pip install -r requirements.txt
