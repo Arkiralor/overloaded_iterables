5eb703bf95ebe4a446639a1b09870f825bf92055
Arkiralor <prithoo11335@gmail.com>
2023-04-30 06:50:01 +0530

    long desc

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='overloaded-iterables',
-    version='0.5.2',
+    version='0.5.3',
     description="Overloaded version of the built-in python classes: <list> and <set> to include some extra functionalities.",
+    long_description="Overloaded version of the built-in python classes: <list> and <set> to include some extra functionalities such as"\
+        "<class>.mean(), <class>.rms(), <class>.raise_to(), etc.",
     license='MIT',
     author="Prithoo Medhi",
     author_email='prithoo11335@gmail.com',
